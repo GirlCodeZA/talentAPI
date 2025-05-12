61ff060e478d58a01dae4ad1cb2bbef1e010684f
Nsovo <manganyins@gmail.com>
2025-05-12 06:34:08 +0200

    WIP

--- app/models/shared.py
+++ app/models/shared.py
@@ -14,3 +14,20 @@ class ProfileStatus(str, Enum):
     INCOMPLETE = "incomplete"
     VERIFIED = "verified"
     ARCHIVED = "archived"
+
+
+class ApplicationStatus(str, Enum):
+    INACTIVE = "inactive"           # Covers DEACTIVATED, DELETED, ARCHIVED
+    PROFILE_INCOMPLETE = "profile_incomplete"
+    PROFILE_PENDING = "profile_pending_verification"
+    PROFILE_VERIFIED = "profile_verified"
+    PROFILE_SUSPENDED = "profile_suspended"
+
+    APPLICATION_PENDING = "application_pending"
+    APPLICATION_VIEWED = "application_viewed"
+    APPLICATION_ACCEPTED = "application_accepted"
+    APPLICATION_DECLINED = "application_declined"
+    INTERVIEW_STAGE = "interview_stage"
+    OFFER_EXTENDED = "offer_extended"
+    APPLICATION_REJECTED = "application_rejected"
+    CANDIDATE_HIRED = "candidate_hired"
